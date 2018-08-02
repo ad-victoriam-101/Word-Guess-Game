f67f05a018246e51230b82b9357b3c887c7a6de8
Ad_Victoriam <keeper24@gmail.com>
2018-08-01 22:29:22 -0700

    Added most of the Java Script, and a bunch of small fixes to html. also created a Css file.

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -3,12 +3,10 @@ window.onload = function () {
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
           'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
           't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    
-    var categories;         // Array of topics
-    var chosenCategory;     // Selected catagory
+    var possibleWords =["apple","diamonds","cake","books","animals","rainbow"];
     var getHint ;          // Word getHint
     var word ;              // Selected word
-    var guess ;             // Geuss
+    var guess ;             // Guess
     var guesses = [ ];      // Stored geusses
     var lives ;             // Lives
     var counter ;           // Count correct geusses
@@ -16,7 +14,64 @@ window.onload = function () {
   
     // Get elements
     var showLives = document.getElementById("mylives");
-    var showCatagory = document.getElementById("scatagory");
-    var getHint = document.getElementById("hint");
-    var showClue = document.getElementById("clue");
+    var underscore = document.getElementById("wordUnderscore");
+
+  // constructing a queryURL variable we will use instead of the literal string inside of the ajax method
+
+//   var queryURL = 'https://od-api.oxforddictionaries.com:443/api/v1/wordlist/en/registers=Rare;domains=Art';
+
+//   $.ajax({
+//     url: 'https://cors-anywhere.herokuapp.com/' + queryURL,
+//     headers: {'app_id': '60c32d7d', 'app_key': '337bc51bbead6cc72908d18645b94bf9'},
+//     method: "GET"
+//   }).then(function(response) {
+//     console.log(response);
+//   });
+    var buttons = function () {
+        myButtons = document.getElementById("buttons");
+        letters = document.createElement("ul");
+
+        for (var i = 0; i < alphabet.length; i++) {
+        letters.id = 'alphabet';
+        list = document.createElement('li');
+        list.id = 'letter';
+        list.innerHTML = alphabet[i];
+        //   check();
+        myButtons.appendChild(letters);
+        letters.appendChild(list);
+        }
+    }
+    comments = function () {
+        showLives.innerHTML = "You have " + lives + " lives";
+        if (lives < 1) {
+          showLives.innerHTML = "Game Over";
+        }
+        for (var i = 0; i < guesses.length; i++) {
+          if (counter + space === guesses.length) {
+            showLives.innerHTML = "You Win!";
+          }
+        }
+    }
+    play = function(){
+        word = possibleWords[Math.floor(Math.random()*possibleWords.length)]
+        word = word.replace(/\s/g,"-");
+        for (var i = 0; i < word.length; i++){
+            underscore.innerHTML = " _ "
+            console.log("_")
+            
+        }
+        console.log(word);
+        buttons();
+        guesses = [];
+        lives = 10;
+        counter = 0;
+        space = 0;
+        comments();
+    }
+    play();
+    document.onkeyup = function(event){
+        
+    }
+
+// ending currly
 }
